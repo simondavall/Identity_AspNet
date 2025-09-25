f7c2ce8290b0246ff21c8a26090a609375f6d1f0
Simon Da Vall <simon.davall@gmail.com>
2025-09-25 15:37:49 +0100

    Initial commit

---
+++ Identity_AspNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_AspNet.Data;
+
+public class ApplicationDbContext : IdentityDbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+}
---
+++ Identity_AspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Identity_AspNet.Models;
+
+public class ErrorViewModel {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
